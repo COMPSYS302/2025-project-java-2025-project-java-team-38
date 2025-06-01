6db2660a19e911891ce62792c28056b9d411a3ac
pranit-kancharlapalli <pranit.kancharlapalli@gmail.com>
2025-06-01 22:48:06 +1200

    fixed hamburger, added functionality, reformatting

--- closet_app/app/src/main/java/com/example/closet/DetailsActivity.java
+++ closet_app/app/src/main/java/com/example/closet/DetailsActivity.java
@@ -1,20 +1,82 @@
 package com.example.closet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.ArrayAdapter;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.appcompat.widget.SearchView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import com.google.android.material.navigation.NavigationView;
 
 public class DetailsActivity extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    ActionBarDrawerToggle toggle;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_details); // links to the XML
+        setContentView(R.layout.activity_details);
+
+        // Side window set up
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this,
+                drawerLayout,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+        drawerLayout.addDrawerListener(toggle);
+
+        // Hamburger click set up
+        ImageView hamburger = findViewById(R.id.hamburger_icon);
+        hamburger.setOnClickListener(v -> drawerLayout.openDrawer(GravityCompat.START));
+
+        // Navigating menu via clicks
+        NavigationView navigationView = findViewById(R.id.navigation_view);
+        navigationView.setNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.nav_top_picks || id == R.id.nav_most_viewed || id == R.id.nav_new_in) {
+                startActivity(new Intent(DetailsActivity.this, MainActivity.class));
+            }
+            drawerLayout.closeDrawers();
+            return true;
+        });
+
+        // Click logo or title sends to home page
+        TextView title = findViewById(R.id.logo_title);
+        ImageView logo = findViewById(R.id.logo_icon);
+        View.OnClickListener goHome = v -> {
+            Intent intent = new Intent(DetailsActivity.this, MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            startActivity(intent);
+        };
+        title.setOnClickListener(goHome);
+        logo.setOnClickListener(goHome);
+
+        // Dropdown for sizes
+        Spinner sizeSpinner = findViewById(R.id.size_spinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this,
+                R.array.sizes_array,
+                android.R.layout.simple_spinner_item
+        );
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        sizeSpinner.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) return true;
+        return super.onOptionsItemSelected(item);
     }
 }
